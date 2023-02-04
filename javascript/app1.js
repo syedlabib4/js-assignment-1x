75d07e2fe230d4a09415174e421e39ee1533e190
syedlabib4 <124432659+syedlabib4@users.noreply.github.com>
2023-02-04 15:16:57 +0500

    Add files via upload

---
+++ javascript/app1.js
@@ -0,0 +1,64 @@
+//chapter 2
+
+//Q1
+var username ="labib";
+console.log ("username")
+//Q2
+var myName="syedlabibbukhari"; 
+console.log ("myName")
+//Q3
+var message;
+message="helloworld";
+console.log(message)
+//Q4
+var student= {
+    firstName:"syedLabib",
+    lastName:"bukhari",
+    age:"19",
+    education:"software engineering"
+};
+alert("firstName:"+student.firstName);
+alert("lastName:"+student.lastName);
+alert("age:"+student.age);
+alert("education:"+student.education);
+//Q5
+var message ="pizza";
+alert(message);
+//Q6
+var email="labibbukhari@gmail.com";
+alert("email:"+ email )
+//Q7
+var book="a smarter way to learn java script"
+alert("book" +book)
+//Q8
+document.write("<h1>Yah!i can write HTML content through java script</h1>")
+//Q9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+//chapter 3
+//Q1
+ var age="19";
+ alert("age" + age);
+//Q2
+ var a='“you have visited this site 16 times”.';
+ alert(a)
+ alert("age" + age);
+//Q3
+ var birthyear="2003";
+ alert("birthyear:" + birthyear);
+ document.write("<h1>my birth year is 2003 </br>date type of my declared variable is number</h1>")
+ document.write("<h1>“JohnDoe ordered 5 T-shirt(s) on XYZ Clothing store”.</h1>")
+ 
+ //chapter 4
+ //Q1
+ var name ="labib";age=19;city="newyork";
+ //Q2
+
+
+ //Q3
+ document.write("<h1>“Rules for naming JS variables”</h1>")
+ document.write("Variable names can only contain .numbers $,and .for example:$my 1st variable,.For example $my_1stVariable </br>Variables must begin with a letter$, or_. For example $name, _name or name</br> Variable names are case sensitive</br>Variable names should not be JS keywords")
+
+ 
+
